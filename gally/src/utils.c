32df4ebe7d50827907c297b16c7743e930f18f3b
coni <46821134+coni@users.noreply.github.com>
2023-01-15 14:30:30 +0100

    system_Exec, to start a process on windows and linux

--- gally/src/utils.c
+++ gally/src/utils.c
@@ -1,6 +1,8 @@
 #ifdef _WIN32
 #include <direct.h>
 #include <io.h>
+#include <windows.h>
+#include <tchar.h>
 #endif
 
 #ifdef __unix__
@@ -134,7 +136,41 @@ int http_Download(char* url, char* filename)
 
 int system_Exec(char* command)
 {
-    return system(command);
+    #ifdef _WIN32
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    // Start the child process. 
+    if (!CreateProcessA(NULL,   // No module name (use command line)
+        command,        // Command line
+        NULL,           // Process handle not inheritable
+        NULL,           // Thread handle not inheritable
+        FALSE,          // Set handle inheritance to FALSE
+        0,              // No creation flags
+        NULL,           // Use parent's environment block
+        NULL,           // Use parent's starting directory 
+        &si,            // Pointer to STARTUPINFO structure
+        &pi)           // Pointer to PROCESS_INFORMATION structure
+        )
+    {
+        printf("CreateProcess failed (%d).\n", GetLastError());
+        return 1;
+    }
+
+    // Wait until child process exits.
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    // Close process and thread handles. 
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    #elif __unix__
+    system(command);
+    #endif
+    return 0;
 }
 
 int system_Mkdir(char* dir) 
