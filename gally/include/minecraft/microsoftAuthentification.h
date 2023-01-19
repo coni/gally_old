613b2a6a5ae815cee9e102aed31ba77d2065353b
coni <46821134+coni@users.noreply.github.com>
2023-01-19 18:28:42 +0000

    get Minecraft UUID

--- gally/include/minecraft/microsoftAuthentification.h
+++ gally/include/minecraft/microsoftAuthentification.h
@@ -2,3 +2,6 @@
 
 char* accessToken();
 char* xblToken(char* accessToken);
+void xstsToken(char* xbToken, char** xstsToken, char** uhs);
+char* mcToken(char* xstsToken, char* uhs);
+char* mc_GetUUID(char* username);
--- gally/src/minecraft/microsoftAuthenfication.c
+++ gally/src/minecraft/microsoftAuthenfication.c
@@ -100,3 +100,69 @@ char* xblToken(char* accessToken)
     free(value_token);
     return token;
 }
+
+void xstsToken(char* xbToken, char** xstsToken, char** uhs)
+{
+    cJSON* tmp_i = NULL;
+
+    cJSON* payload = cJSON_CreateObject();
+
+    cJSON* tmp = cJSON_CreateString("rp://api.minecraftservices.com/");
+    cJSON_AddItemToObject(payload, "RelyingParty", tmp);
+
+    tmp = cJSON_CreateString("JWT");
+    cJSON_AddItemToObject(payload, "TokenType", tmp);
+
+    tmp = cJSON_CreateObject();
+
+    tmp_i = cJSON_CreateString("RETAIL");
+    cJSON_AddItemToObject(tmp, "SandboxId", tmp_i);
+
+    tmp_i = cJSON_CreateArray();
+    cJSON_AddItemToArray(tmp_i, cJSON_CreateString(xbToken));
+    cJSON_AddItemToObject(tmp, "UserTokens", tmp_i);
+    cJSON_AddItemToObject(payload, "Properties", tmp);
+
+    http_Response response = http_Post("https://xsts.auth.xboxlive.com/xsts/authorize", cJSON_Print(payload), "Content-Type: application/json");
+    cJSON* responseJson = cJSON_Parse(response.data);
+    tmp = cJSON_GetObjectItemCaseSensitive(responseJson, "Token");
+    *xstsToken = tmp->valuestring;
+    tmp = cJSON_GetObjectItemCaseSensitive(responseJson, "DisplayClaims");
+    tmp = cJSON_GetObjectItemCaseSensitive(tmp, "xui");
+    tmp = cJSON_GetArrayItem(tmp, 0); 
+    tmp = cJSON_GetObjectItemCaseSensitive(tmp, "uhs");
+    *uhs = tmp->valuestring;
+}
+
+char* mcToken(char* xstsToken, char* uhs)
+{
+    cJSON* responseJson = NULL;
+    cJSON* tmp = NULL;
+
+    size_t len_identityToken = (strlen(xstsToken) + strlen(uhs) + 11);
+    char* identityToken = malloc(sizeof(char) * len_identityToken);
+    snprintf(identityToken, len_identityToken, "XBL3.0 x=%s;%s", uhs, xstsToken);
+    cJSON* payload = cJSON_CreateObject();
+
+    tmp = cJSON_CreateString(identityToken);
+    cJSON_AddItemToObject(payload, "identityToken", tmp);
+
+    http_Response response = http_Post("https://api.minecraftservices.com/authentication/login_with_xbox", cJSON_Print(payload), "Content-Type: application/json");
+    responseJson = cJSON_Parse(response.data);
+    tmp = cJSON_GetObjectItemCaseSensitive(responseJson, "access_token");
+
+    return tmp->valuestring;
+}
+
+char* mc_GetUUID(char* username)
+{
+    char* url = malloc(sizeof(char) * (strlen(username) + 49));
+    strcpy(url, "https://api.mojang.com/users/profiles/minecraft/");
+    strcat(url, username);
+    http_Response response = http_Get(url); 
+    cJSON* responseJson = cJSON_Parse(response.data);
+    cJSON* tmp = cJSON_GetObjectItemCaseSensitive(responseJson, "id");
+    username = tmp->valuestring;
+    cJSON_free(responseJson);
+    return username;
+}
